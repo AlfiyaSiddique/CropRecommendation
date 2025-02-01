0aa295e1124f7b2a60fa87f50c1e6051f0eef471
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2025-02-01 16:59:36 +0530

    Tailwind error fix

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,8 +4,8 @@ import "./global.css"
 
 export default function App() {
   return (
-    <View className="bg-red-500">
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="bg-blue-500 mt-28 p-4">
+      <Text className='text-white'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
